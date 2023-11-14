48ba25ee23508037a04c7b221393153080cc4b79
hemypontes <130715921+hemypontes@users.noreply.github.com>
2023-11-14 19:46:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ alert('Olá, Mundo!');
 function <!-- colocar o nome do som, no slide como TOCASOMPOM  --> (){
     document.querySelector <!--('#som_tecla_pom')-->. play();
 }
+document.querySelector(‘.tecla_pom’).onclick ;
